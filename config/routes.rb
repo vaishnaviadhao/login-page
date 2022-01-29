f7d47946601e35efa676a1514be23d0892a58026
Vaishnavi <vaishnaviadhao33@gmail.com>
2022-01-29 22:09:01 +0530

    Your message about the commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :users
+    root 'dashboard#index'
+  get 'pages/index'
+  # root to: "pages#index"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
